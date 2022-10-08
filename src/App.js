f39d63f41f9844804807871ebbebc95045ba3c39
Manuch Sadri <colonquebang@hotmail.com>
2022-10-07 23:53:18 -0700

    completed user story 2 with form, API call, and Location component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-// import Container from 'react-bootstrap/Container';
-// import Form from 'react-bootstrap/Form';
-// import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+import Location from './Location';
 
 class App extends React.Component {
 
@@ -13,53 +14,58 @@ class App extends React.Component {
             userCity: '',
             location: {}
         }
-    }
+    };
 
     getCity = (event) => {
         this.setState({ userCity: event.target.value });
     };
 
+    // getKeyPress = (event) => {
+    //     event.preventDefault();
+    //     console.log(event.key);
+    //     if(event.key === 'Enter'){
+    //         this.searchCity();
+    //     }
+    // };
+
     searchCity = async () => {
-        const locIq = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.userCity}&format-json`;
-        console.log('api url: ', locIq);
+        const locIq = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.userCity}&format=json`;
         const response = await axios.get(locIq);
-        console.log('response object: ', response);
-        console.log('response.data[0]: ', response.data[0]);
         this.setState({location: response.data[0]});
-        console.log('userCity: ', this.userCity);
     };
 
     render() {
         return (
             <div className="App">
                 <h1>City Explorer</h1>
-                {/* <Container>
+                <Container>
                     <Form>
                         <Form.Group className="mb-3">
                             <Form.Label>Enter a city name</Form.Label>
                             <Form.Control
+                                onChange={this.getCity}
                                 type="text"
                                 placeholder="Seattle"
-                                onChange={this.getCity}
-                            />
+                                // onKeyPress={this.getKeyPress} 
+                             />
                         </Form.Group>
+                        <Button
+                            onClick={this.searchCity}
+                            variant="outline-primary"
+                            size="sm" >
+                            Explore!
+                        </Button>
                     </Form>
-                    <Button
-                        onClick={this.searchCity}
-                        variant="outline-primary"
-                        size="sm" >
-                        Explore!
-                    </Button>
-                </Container> */}
-                <label>Enter a city name to explore:</label>
+
+                </Container>
                 <br/>
-                <input
-                    type="text"
-                    onChange={this.getCity}
-                    placeholder="Seattle"
-                />
-                <br/>
-                <button onClick={this.searchCity}>Explore!</button>
+                <div>
+                    {this.state.location.display_name &&
+                        <Location
+                            location = {this.state.location}
+                        />
+                    }
+                </div>
             </div >
         );
     }
